3b3ef4d465555cd30187b7b9267cfa1aad906989
WorkSutton <work.sutton.notifications@gmail.com>
2023-06-15 16:24:08 +0100

    Add a page title
     - Defaults to 'Homey' if nothing provided to helper
     - Comprised of 'Homey - <resource> - <model attribute>' if both provided to helper

--- spec/system/creating_projects_spec.rb
+++ spec/system/creating_projects_spec.rb
@@ -6,12 +6,18 @@ RSpec.feature "Users can create new projects" do
 
     click_link "New Project"
 
-    fill_in "Title", with: "Rogers - 100 Lochabar Road, SW3 1QQ"
+    fill_in "Title", with: "Rogers @ 100 Lochabar Road, London, SW3 1QQ"
     fill_in "Description", with: "Lorum imspum text"
 
     click_button "Create Project"
 
     expect(page).to have_content "Project has been created."
-    expect(page).to have_content "Rogers - 100 Lochabar Road, SW3 1QQ"
+    expect(page).to have_content "Rogers @ 100 Lochabar Road, London, SW3 1QQ"
+
+    project = Project.find_by(title: "Rogers @ 100 Lochabar Road, London, SW3 1QQ")
+    expect(page.current_url).to eq project_url(project)
+
+    title = "Homey - Projects - Rogers @ 100 Lochabar Road, London, SW3 1QQ"
+    expect(page).to have_title title
   end
 end
