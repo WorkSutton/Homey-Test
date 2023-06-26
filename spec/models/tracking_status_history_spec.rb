c431e4befbc36575b3cc9d1c4b0dd2d2599d3536
WorkSutton <work.sutton.notifications@gmail.com>
2023-06-26 07:59:46 +0100

    Refactor TrackingStatusHistory spec

--- spec/models/tracking_status_history_spec.rb
+++ spec/models/tracking_status_history_spec.rb
@@ -1,98 +1,127 @@
 require "rails_helper"
 
 RSpec.describe TrackingStatusHistory, type: :model do
+  def history
+    history = TrackingStatusHistory.where(project_id: project.reload.id).order(id: :asc)
+    current_history, previous_history = history.partition do |record|
+      record.history_ended_at.nil?
+    end
+    return [history, current_history, previous_history]
+  end
+
   context "with a new project on creation" do
-    let(:new_project) { FactoryBot.build(:project) }
+    let(:project) { FactoryBot.build(:project, title: "Project testing Creation") }
+    let(:project_history) { history[0] }
+
+    specify do
+      expect { project.save }.to change(TrackingStatusHistory, :count).by(1)
+    end
 
     it "defaults the status to 'draft'" do
-      expect(new_project.tracking_status).to eq "draft"
-      expect { new_project.save }.to change(TrackingStatusHistory, :count).by(1)
+      project.save
+
+      expect(project.tracking_status).to eq "draft"
       expect(TrackingStatusHistory.last).to have_attributes(
         tracking_status: "draft",
-        project_id: new_project.reload.id
+        project_id: project.reload.id
       )
-
-      project_tracking_status = TrackingStatusHistory.find_by(project_id: new_project.reload.id)
-      expect(project_tracking_status.tracking_status).to eq "draft"
-      expect(project_tracking_status.history_ended_at).to be_nil
-      expect(project_tracking_status.history_started_at).to eq new_project.updated_at
+      expect(project_history[0].tracking_status).to eq "draft"
+      expect(project_history[0].history_ended_at).to be_nil
+      expect(project_history[0].history_started_at).to eq project.updated_at
     end
   end
 
-  context "when an update is made to the project's tracking_status" do
-    let!(:saved_project) { FactoryBot.build(:project) }
-    let!(:original_project_creation_time) { Time.zone.local(2023, 06, 22, 12, 44, 00) }
+  context "when updates are made to a project" do
+    let(:project) { FactoryBot.build(:project, title: "Project testing Updates") }
+    let(:june_22nd_2023_at_101505) { Time.zone.local(2023, 06, 22, 10, 15, 05) }
+    let(:june_23th_2023_at_111505) { Time.zone.local(2023, 06, 23, 11, 15, 05) }
+    let(:june_24th_2023_at_152040) { Time.zone.local(2023, 06, 24, 15, 20, 40) }
+
     before do
-      travel_to original_project_creation_time
-      saved_project.save # = FactoryBot.create(:project)
+      travel_to june_22nd_2023_at_101505
+      project.save # = FactoryBot.create(:project)
     end
 
     after do
       travel_back
     end
 
-    it "records history" do
-      june_24th_2023_at_090444 = Time.zone.local(2023, 06, 24, 9, 4, 44)
-      travel_to june_24th_2023_at_090444
+    context "when tracking_status changes to 'approved' from 'draft'" do
+      let(:project_history) { history[0] }
+      let(:current_history) { history[1] }
+      let(:previous_history) { history[2] }
 
-      expect { saved_project.approved! }.to change(TrackingStatusHistory, :count).by(1)
-
-      history_for_saved_project = TrackingStatusHistory.where(project_id: saved_project.reload.id)
-      expect(history_for_saved_project.size).to eq(2)
-
-      # puts "\n\n   ----- Inspection time -----\nNo. records: #{history_for_saved_project.size}\n"
-      # history_for_saved_project.each do |p|
-      #   puts "#{p.inspect}"
-      # end
-      # puts "\n   ----- END -----------------\n\n"
-
-      current_history, previous_history = history_for_saved_project.partition do |record|
-        record.history_ended_at.nil?
-      end
-      expect(current_history.first).to have_attributes(
-        tracking_status: "approved",
-        project_id: saved_project.reload.id,
-        history_started_at: Time.current,
-        history_ended_at: nil
-      )
-
-      june_24th_2023_at_103139 = Time.zone.local(2023, 06, 24, 10, 31, 39)
-      travel_to june_24th_2023_at_103139
-
-      saved_project.in_progress!
-      history_for_saved_project = TrackingStatusHistory.where(project_id: saved_project.reload.id)
-      expect(history_for_saved_project.size).to eq(3)
-
-      current_history, previous_history = history_for_saved_project.partition do |record|
-        record.history_ended_at.nil?
+      before do
+        travel_to june_23th_2023_at_111505
+        project.approved!
       end
 
-      expect(current_history.first).to have_attributes(
-        tracking_status: "in_progress",
-        project_id: saved_project.reload.id,
-        history_started_at: Time.current,
-        history_ended_at: nil
-      )
+      specify { expect(history[0].size).to eq(2) }
+      specify { expect(project_history.size).to eq(2) }
 
-      expect(previous_history.first).to have_attributes(
-        tracking_status: "draft",
-        project_id: saved_project.reload.id,
-        history_started_at: original_project_creation_time,
-        history_ended_at: june_24th_2023_at_090444
-      )
+      it "updates 'history_ended_at' for the current record" do
+        expect(previous_history.first).to have_attributes(
+          tracking_status: "draft",
+          project_id: project.id,
+          history_started_at: june_22nd_2023_at_101505,
+          history_ended_at: june_23th_2023_at_111505
+        )
+      end
 
-      expect(previous_history.last).to have_attributes(
-        tracking_status: "approved",
-        project_id: saved_project.reload.id,
-        history_started_at: june_24th_2023_at_090444,
-        history_ended_at: june_24th_2023_at_103139
-      )
+      it "creates a new record" do
+        expect(current_history.first).to have_attributes(
+          tracking_status: "approved",
+          project_id: project.reload.id,
+          history_started_at: Time.current,
+          history_ended_at: nil
+        )
+      end
+    end
 
-      previous_history_tracking_statuses = previous_history.map(&:tracking_status)
-      expected_tracking_statuses = %w(draft approved)
+    context "when 'tracking_status' changes from 'approved' to 'in progress'" do
+      let(:project_history) { history[0] }
+      let(:current_history) { history[1] }
+      let(:previous_history) { history[2] }
 
-      expect(previous_history_tracking_statuses).to match(expected_tracking_statuses)
-      expect(previous_history_tracking_statuses).not_to match("in_progress")
+      before do
+        travel_to june_23th_2023_at_111505
+        project.approved!
+
+        travel_to june_24th_2023_at_152040
+        project.in_progress!
+      end
+
+      specify { expect(project_history.size).to eq(3) }
+
+      it "updates 'history_ended_at' for the current record" do
+        expect(previous_history.last).to have_attributes(
+          tracking_status: "approved",
+          project_id: project.reload.id,
+          history_started_at: june_23th_2023_at_111505,
+          history_ended_at: june_24th_2023_at_152040
+        )
+      end
+
+      it "doesn't change the 'draft' history record" do
+        expect(previous_history.first).to have_attributes(
+          tracking_status: "draft",
+          project_id: project.reload.id,
+          history_started_at: june_22nd_2023_at_101505,
+          history_ended_at: june_23th_2023_at_111505
+        )
+      end
+
+      it "creates a new 'in_progress' record" do
+        expect(current_history.first).to have_attributes(
+          tracking_status: "in_progress",
+          project_id: project.reload.id,
+          history_started_at: Time.current,
+          history_ended_at: nil
+        )
+      end
+
+      specify { expect(previous_history.map(&:tracking_status)).to match(%w(draft approved)) }
+      specify { expect(previous_history.map(&:tracking_status)).to_not match("in_progress") }
     end
   end
 end
